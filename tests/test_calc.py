7fcbf4dceff0e73abdaf2a932e5bfd807a93f48d
Brij thota <brijthota@brijs-air.resource.campus.njit.edu>
2020-03-02 20:15:47 -0500

    update

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -4,30 +4,25 @@ import unittest
 
 
 class test_PythonSimpleCalc(unittest.TestCase):
-    	def test_calc(self):
-        	c = Calculator()
-        	assert c
-
-    	def test_calc_add(self):
-        	c = Calculator()
-        	assert c.add(3, 2) == 5
-
-    	def test_calc_subtract(self):
-        	c = Calculator()
-        	assert c.subtract(0, 10) == -10
-
-    	def test_calc_multiply(self):
-        	c = Calculator()
-        	assert c.multiply(2, 2) == 4
-
-    	def test_calc_divide(self):
-        	c = Calculator()
-        	assert c.divide(10, 5) == 2
-
+	def test_calc(self):
+		c = Calculator()
+		assert c
+	def test_calc_add(self):
+		c = Calculator()
+		assert c.add(3, 2) == 5
+	def test_calc_subtract(self):
+		c = Calculator()
+		assert c.subtract(0, 10) == -10
+	def test_calc_multiply(self):
+		c = Calculator()
+		assert c.multiply(2, 2) == 4
+	def test_calc_divide(self):
+		c = Calculator()
+		assert c.divide(10, 5) == 2
 	def test_calc_square(self):
-        	c = Calculator()
-        	assert c.square_number(5) == 25
+		c = Calculator()
+		assert c.square_number(5) == 25
 
 	def test_calc_squareroot(self):
-        	c = Calculator()
-        	assert c.square_root(25) == 5
+		c = Calculator()
+		assert c.square_root(25) == 5
